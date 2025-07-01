0a94ff1f1596c420383dcdcdfaa42873444eab24
Akshat Giri <giriakshat95@gmail.com>
2025-07-01 16:02:41 +0530

    commit change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import Weather from './Weather'
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-2xl font-black text-green-500 text-center p-20'>Weather App</h1>
-      <Weather/>
-      
-
+      <h1 className='text-2xl font-black text-green-500 text-center p-20'>App</h1>
     </div>
   )
 }
