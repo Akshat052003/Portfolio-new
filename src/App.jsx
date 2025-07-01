c442c4d254bf8fcf454bd5405c5c699a60ecc5a3
Akshat Giri <giriakshat95@gmail.com>
2025-07-01 16:05:31 +0530

    hello jio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Weather from './Weather'
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-2xl font-black text-green-500 text-center p-20'>App</h1>
+      <h1 className='text-2xl font-black text-green-500 text-center p-20'>Weather App</h1>
+      <Weather/>
+      
+
     </div>
   )
 }
